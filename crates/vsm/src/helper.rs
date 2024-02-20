9e1171da7c8d90639e0ed175e9c321af947e8190
Vixen <infvix@gmail.com>
2024-02-20 23:00:53 +0100

    Fix hot reload

--- crates/vsm/src/helper.rs
+++ crates/vsm/src/helper.rs
@@ -1,6 +1,8 @@
 use axum::{body::Body, extract::Request};
 
+#[allow(unused_variables)]
 pub fn accept_gzip(request: &Request<Body>) -> bool {
+    #[cfg(not(debug_assertions))]
     match request.headers().get("Accept-Encoding") {
         Some(header) => match header.to_str() {
             Ok(str) => str.starts_with("gzip"),
@@ -8,6 +10,9 @@ pub fn accept_gzip(request: &Request<Body>) -> bool {
         },
         None => false,
     }
+
+    #[cfg(debug_assertions)]
+    false
 }
 
 pub fn accept_gzip_include_mime(mime: &str, request: &Request<Body>) -> bool {
